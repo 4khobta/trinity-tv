b2a1e9176bb7832ab15b313b2d1f37eb2ea4bdee
khobta <a@khobta.com>
2022-07-25 22:25:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,52 @@
+# TrinityTV API
+
+```php
+require_once 'TrinityTV.class.php';
+//YOUR_PARTNER_ID,YOUR_SALT_CODE,TARIFF_ID - видається менеджерами https://trinity-tv.net
+$api = new TrinityTV('YOUR_PARTNER_ID', 'YOUR_SALT_CODE');
+
+// Створити підписку, де 123 - ваш внутрішній номер абонента
+$api->create(123, TARIFF_ID);
+//Інформація про підписку абнента 123
+$user = $api->getUser(123);
+//Додати мак пристрою з приміткою або без
+$api->addDeviceByMac(123, '112233445566', 'Примітка');
+//Додати пристрій по коду з приміткою або без
+$api->addDeviceByCode(123, 9999, 'Примітка');
+//Видалити пристрій по маку, можна по UUID
+$api->deleteDevice(123, '112233445566');
+$api->deleteDevice(123, '', 'uuid-id');
+$api->deleteDevice(123, uuid: 'uuid-id');
+//Оновити примітку до пристрою, де 12345 - ID пристрою
+$api->updateDeviceNote(12345, 'Примітка');
+//Призупитити підписку абонета
+$api->suspend(123);
+//Відновити підписку абонета
+$api->resume(123);
+//Змінити дані про абонента
+$api->updateUser(123, 'Ім`я', 'Прізвище', 'По батькові', 'Адреса');
+//Список всіх підписок
+$users = $api->usersList();
+//Отримати звіт за останній місяць
+$report = $api->getReport();
+// Отримати підключені пристрої абонета 123
+$devices = $api->getDevices(123);
+//Отримати всі пристрої підписок
+$all_devices = $api->getDeviceList();
+//Змінити номер договору (внутрішній)
+$api->changeContract(123, 1234);
+//Отримати останню сесію абонентів
+$sessions = $api->getLastSessions();
+//Створити плейлист m3u для абонента
+$playlist = $api->getPlaylist(123);
+//Видалити плейлист
+$api->deletePlaylist(123, 'playlist_url'); // Або $this->deleteDevice(123, uuid: 'playlist_url');
+//Отримати лінк для автоматичної асторизації на сайті трініті, 1 - https://trinity-tv.net, 2 - https://sweet.tv/
+$link = $api->getLink(123, 1);// 1 - по дефолту
+//Отримати назву типу пристрою з масиву devices['device_type'] від $api->getDevices(123) або $api->getDeviceList()
+$type = $api->getDeviceType(8);// Поверне DT_MAG250_Micro
+
+if ($api->errors) {
+    print_r($error); // Масив з помилками
+}
+```
---
+++ TrinityTV.class.php
@@ -0,0 +1,226 @@
+<?php
+
+class TrinityTV
+{
+    private int $partner_id;
+    private string $salt;
+    private string $api_url = 'http://partners.trinity-tv.net/partners/user/';
+    public array $errors = [];
+
+    public function __construct($partner_id, $salt)
+    {
+        $this->partner_id = $partner_id;
+        $this->salt = $salt;
+    }
+
+    private function client(string $url): string
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $out = curl_exec($ch);
+        curl_close($ch);
+        return $out;
+    }
+
+    private function execute(string $method, array $params = [])
+    {
+        $url = $this->api_url . $method;
+        $params = array_merge([
+            'requestid' => hexdec(uniqid()),
+            'partnerid' => $this->partner_id,
+        ], $params);
+
+        $hash_params = $params;
+
+        array_walk($hash_params, function (&$val, $key) {
+            if (in_array($key, ['firstname', 'lastname', 'middlename', 'address']))
+                $val = urlencode($val);
+        });
+
+        unset($hash_params['note']);
+        $data = array_merge($params, ['hash' => md5(implode($hash_params) . $this->salt)]);
+        $response = json_decode($this->client($url . '?' . http_build_query($data)), true);
+        if ($response && isset($response['result'])) {
+            if ($response['result'] == 'success')
+                return $response;
+            else
+                $this->errors[] = $response['result'];
+
+        } else {
+            $this->errors[] = 'Connection error';
+        }
+    }
+
+    public function getUser(int $id): array|null
+    {
+        /*
+         *  subscrstatusid: 0 - active, 768 - blocked
+         */
+        return $this->execute('subscriptioninfo', [
+            'localid' => $id
+        ]);
+    }
+
+    public function getDevices(int $id): array|null
+    {
+        return $this->execute('devicelist', [
+            'localid' => $id
+        ]);
+    }
+
+    public function create(int $id, int $tariff_id): array|null
+    {
+        return $this->execute('create', [
+            'localid' => $id,
+            'subscrid' => $tariff_id
+        ]);
+    }
+
+    public function addDeviceByMac(int $id, $mac = null, $note = null, $uuid = null): array|null
+    {
+        return $this->execute($note ? 'autorizedevice_note' : 'autorizedevice', [
+            'localid' => $id,
+            'mac' => $mac,
+            'uuid' => $uuid,
+            'note' => mb_convert_encoding($note, "Windows-1251", "UTF-8") // WTF trinity ?
+        ]);
+    }
+
+    public function addDeviceByCode(int $id, $code, $note = null): array|null
+    {
+        return $this->execute('autorizebycode', [
+            'localid' => $id,
+            'code' => $code,
+            'note' => $note
+        ]);
+    }
+
+    public function deleteDevice(int $id, $mac = null, $uuid = null): array|null
+    {
+        return $this->execute('deletedevice', [
+            'localid' => $id,
+            'mac' => $mac,
+            'uuid' => $uuid
+        ]);
+    }
+
+    public function updateDeviceNote(int $device_id, string $note): array|null
+    {
+        return $this->execute('updatenotebydevice', [
+            'deviceid' => $device_id,
+            'note' => $note
+        ]);
+    }
+
+    public function suspend(int $id): array|null
+    {
+        return $this->subscribe($id, 'suspend');
+    }
+
+    public function resume(int $id): array|null
+    {
+        return $this->subscribe($id, 'resume');
+    }
+
+    private function subscribe(int $id, string $operation): array|null
+    {
+        return $this->execute('subscription', [
+            'localid' => $id,
+            'operationid' => $operation
+        ]);
+    }
+
+    public function usersList(): array|null
+    {
+        return $this->execute('subscriberlist');
+    }
+
+    public function updateUser(int $id, string $first_name = null, string $last_name = null, string $middle_name = null, string $address = null): array|null
+    {
+        return $this->execute('updateuser', [
+            'localid' => $id,
+            'firstname' => $first_name,
+            'lastname' => $last_name,
+            'middlename' => $middle_name,
+            'address' => $address
+        ]);
+    }
+
+    public function getReport(): array|null
+    {
+        return $this->execute('listreportupdated');
+    }
+
+    public function getDeviceList(): array|null
+    {
+        return $this->execute('fulldevicelist');
+    }
+
+    public function changeContract($id, $new_id): array|null
+    {
+        return $this->execute('newcontract', [
+            'localid' => $id,
+            'newcontract' => $new_id
+        ]);
+    }
+
+    public function getLastSessions(): array|null
+    {
+        return $this->execute('getsessionsdate');
+    }
+
+    public function getPlaylist(int $id): array|null
+    {
+        return $this->execute('getplaylist', [
+            'localid' => $id
+        ]);
+    }
+
+    public function deletePlaylist(int $id, string $playlist): array|null // same deleteDevice
+    {
+        return $this->deleteDevice($id, uuid: $playlist);
+    }
+
+    public function getLink(int $id, int $site_id = 1): array|null //Autorize in site site_id: 1 - trinity, 2 - sweet.tv
+    {
+        return $this->execute('siteaccesslink', [
+            'localid' => $id,
+            'siteid' => $site_id
+        ]);
+    }
+
+    public function getDeviceType(int|null|string $type_id): string
+    {
+        /*
+         * $type_id - return from devices['device_type']
+         */
+        $types = [
+            0 => 'DT_Unknown',
+            1 => 'DT_DIB_120',
+            2 => 'DT_IPTV_Player',
+            7 => 'DT_MAG200',
+            8 => 'DT_MAG250_Micro',
+            9 => 'DT_MAG250_Mini',
+            10 => 'DT_Himedia_HD600A',
+            11 => 'DT_Android_Player',
+            12 => 'DT_STB_Emul',
+            13 => 'DT_SmartTV',
+            14 => 'DT_iNext',
+            15 => 'DT_M3U',
+            16 => 'DT_AndroidTV',
+            17 => 'DT_IOS_Player',
+            18 => 'DT_MacOS_Player',
+            19 => 'DT_Kivi_TV',
+            20 => 'DT_GX_STB',
+            21 => 'DT_NOMI_TV',
+            22 => 'DT_Web_Browser',
+            23 => 'DT_ERGO_TV',
+            24 => 'DT_AppleTV',
+            25 => 'DT_Xbox'
+        ];
+        if (array_key_exists($type_id, $types))
+            return $types[$type_id];
+        return 'Unknown';
+    }
+}
